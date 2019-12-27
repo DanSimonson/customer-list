3af541943dd19d6b9237a0d7105e287ddb33b7ca
DanSimonson <dansimonson@mariposaweb.net>
2019-12-27 11:44:53 -0700

    initial commit for styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import "./Table.css";
 function Table({ data }) {
-  /*if (data[0]) {
-    //return null;
-    console.log("props:", data[0].firstName);
-    console.log("props:", data[1].email);
-  }*/
-
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>User-Id</th>
-          <th>First-Name</th>
-          <th>Last-Name</th>
-          <th>Email</th>
-          <th>Cell-Phone</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(row => (
-          <tr key={row.userID}>
-            <td>{row.userID}</td>
-            <td>{row.firstName}</td>
-            <td>{row.lastName}</td>
-            <td>{row.email}</td>
-            <td>{row.cellPhone}</td>
-            <td>{row.status}</td>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>User-Id</th>
+            <th>First-Name</th>
+            <th>Last-Name</th>
+            <th>Email</th>
+            <th>Cell-Phone</th>
+            <th>Status</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map(row => (
+            <tr key={row.userID}>
+              <td>{row.userID}</td>
+              <td>{row.firstName}</td>
+              <td>{row.lastName}</td>
+              <td>{row.email}</td>
+              <td>{row.cellPhone}</td>
+              <td>{row.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 }
 Table.propTypes = {
