2ee25500684428b1e50cee1d74e48b0343b2ff86
DanSimonson <dansimonson@mariposaweb.net>
2019-12-30 21:15:04 -0700

    added ability to toggle boolean props in parent from child (child -> parent)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showMenu: false,
       data: []
     };
   }
+  editToggle = () => {
+    this.setState(
+      prevState => ({
+        showMenu: !prevState.showMenu
+      }),
+      () => {
+        console.log("showMenu: ", this.state.showMenu);
+      }
+    );
+    /*this.setState({ showMenu: !this.showMenu }, () => {
+      console.log("showMenu: ", this.state.showMenu);
+    });*/
+  };
+
   componentDidMount() {
     const docData = [];
     let documentArray = [];
@@ -30,10 +45,15 @@ class App extends Component {
         console.log("Error getting document:", error);
       });
   }
+
   render() {
     return (
       <div className="App">
-        <Table data={this.state.data} />
+        <Table
+          data={this.state.data}
+          showMenu={this.state.showMenu}
+          editToggle={this.editToggle}
+        />
       </div>
     );
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,53 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Table.css";
 
-function Table({ data }) {
-  let showStatus;
-
-  const handleEdit = () => {};
-
-  return (
-    <main>
-      <table>
-        <thead>
-          <tr>
-            <th>User-Id</th>
-            <th>First-Name</th>
-            <th>Last-Name</th>
-            <th>Email</th>
-            <th>Cell-Phone</th>
-            <th>Status</th>
-            <th className="editIcon">
-              <FontAwesomeIcon icon="user-plus" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={row.userID}>
-              <td>{row.userID}</td>
-              <td>{row.firstName}</td>
-              <td>{row.lastName}</td>
-              <td>{row.email}</td>
-              <td>{row.cellPhone}</td>
-              <td>
-                {row.status ? (
-                  <FontAwesomeIcon icon="thumbs-up" />
-                ) : (
-                  <FontAwesomeIcon icon="thumbs-down" />
-                )}
-              </td>
-              <td className="editIcon" onClick={handleEdit}>
-                <FontAwesomeIcon icon="edit" />
-              </td>
+class Table extends Component {
+  //console.log("props: ", this.props.data);
+  handleEdit = () => {
+    this.props.editToggle();
+    //console.log("showMenu: ", showMenu);
+  };
+  render() {
+    return (
+      <main>
+        <table>
+          <thead>
+            <tr>
+              <th>User-Id</th>
+              <th>First-Name</th>
+              <th>Last-Name</th>
+              <th>Email</th>
+              <th>Cell-Phone</th>
+              <th>Status</th>
+              <th className="editIcon">
+                <FontAwesomeIcon icon="user-plus" />
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </main>
-  );
+          </thead>
+          <tbody>
+            {this.props.data.map((row, index) => (
+              <tr key={row.userID}>
+                <td>{row.userID}</td>
+                <td>{row.firstName}</td>
+                <td>{row.lastName}</td>
+                <td>{row.email}</td>
+                <td>{row.cellPhone}</td>
+                <td>
+                  {row.status ? (
+                    <FontAwesomeIcon icon="thumbs-up" />
+                  ) : (
+                    <FontAwesomeIcon icon="thumbs-down" />
+                  )}
+                </td>
+                <td className="editIcon">
+                  <FontAwesomeIcon icon="edit" onClick={this.handleEdit} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
 }
 Table.propTypes = {
   type: PropTypes.array
