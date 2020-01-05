7fe81bb0ad46bc003b269fc0ed7f359b1ed3ada2
DanSimonson <dansimonson@mariposaweb.net>
2020-01-04 19:10:36 -0700

    began add form

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Add.css";
 
-export default class Add extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Add</h1>
-      </div>
-    );
-  }
-}
+const Add = () => {
+  // useState can be used multiple times for different data
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cellPhone, setCellPhone] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    //addSong(title);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setCellPhone("");
+  };
+  return (
+    <form onSubmit={handleSubmit} className="wrapForm">
+      <label>First Name:</label>
+      <input
+        type="text"
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+      <label>Last Name:</label>
+      <input
+        type="text"
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <label>Email:</label>
+      <input
+        type="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <label>Cell Phone:</label>
+      <input
+        type="tel"
+        value={cellPhone}
+        onChange={e => setCellPhone(e.target.value)}
+      />
+      <label>Active </label>
+      <input id="toggle-on" name="toggle" type="radio" />
+      <label for="toggle-on">Yes </label>
+      <input id="toggle-off" name="toggle" type="radio" />
+      <label for="toggle-off">No</label>
+      <input type="submit" value="add" />
+    </form>
+  );
+};
+
+export default Add;
