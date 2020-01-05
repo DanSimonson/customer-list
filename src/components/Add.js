a1ddab93d32116cc6a515fb43d3713c5447cb149
DanSimonson <dansimonson@mariposaweb.net>
2020-01-05 16:49:51 -0700

    added hooks/useState to add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,52 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Add.css";
 
 const Add = () => {
-  // useState can be used multiple times for different data
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cellPhone, setCellPhone] = useState("");
-  const handleSubmit = e => {
-    e.preventDefault();
-    //addSong(title);
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setCellPhone("");
+  const [toggleOn, setToggleOn] = useState("");
+  const [toggleOff, setToggleOff] = useState("");
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
   };
+
   return (
-    <form onSubmit={handleSubmit} className="wrapForm">
-      <label>First Name:</label>
-      <input
-        type="text"
-        value={firstName}
-        onChange={e => setFirstName(e.target.value)}
-      />
-      <label>Last Name:</label>
-      <input
-        type="text"
-        value={lastName}
-        onChange={e => setLastName(e.target.value)}
-      />
-      <label>Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <label>Cell Phone:</label>
-      <input
-        type="tel"
-        value={cellPhone}
-        onChange={e => setCellPhone(e.target.value)}
-      />
-      <label>Active </label>
-      <input id="toggle-on" name="toggle" type="radio" />
-      <label for="toggle-on">Yes </label>
-      <input id="toggle-off" name="toggle" type="radio" />
-      <label for="toggle-off">No</label>
-      <input type="submit" value="add" />
+    <form onSubmit={handleSubmit}>
+      <div className="wrapOne">
+        <label className="padLabel">
+          First Name:
+          <input
+            className="inputMargin"
+            type="text"
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
+        </label>
+        <label className="padLabel">
+          Last Name:
+          <input
+            className="inputMargin"
+            type="text"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="wrapTwo">
+        <label>
+          Active:
+          <input
+            id="toggle-on"
+            name="toggle"
+            type="radio"
+            value="toggleOn"
+            onChange={e => setToggleOn(e.target.value)}
+          />
+          <label for="toggle-on">Yes</label>
+          <input
+            id="toggle-off"
+            name="toggle"
+            type="radio"
+            value="toggleOff"
+            onChange={e => setToggleOff(e.target.value)}
+          />
+          <label for="toggle-off">No</label>
+        </label>
+      </div>
+      <input type="submit" value="Submit" />
     </form>
   );
 };
