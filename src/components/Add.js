377a92a3fb9b387c610bce4e3633cebcba453205
DanSimonson <dansimonson@mariposaweb.net>
2020-01-05 21:31:42 -0700

    began firestore post

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
+import firebase from "../Firestore";
 import "./Add.css";
 
 const Add = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cellPhone, setCellPhone] = useState("");
   const [toggleOn, setToggleOn] = useState("");
   const [toggleOff, setToggleOff] = useState("");
+  const [data, setData] = useState({ hits: [] });
 
   const handleSubmit = evt => {
     evt.preventDefault();
   };
+
+  useEffect(async () => {
+    const db = firebase.firestore();
+    db.collection("cities")
+      .add({
+        //name: "Tokyo",
+        //country: "Japan"
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+    /*const result = await axios(
+      "https://hn.algolia.com/api/v1/search?query=redux"
+    );
+    setData(result.data);*/
+  });
 
   return (
     <form onSubmit={handleSubmit}>
@@ -21,6 +44,7 @@ const Add = () => {
             type="text"
             value={firstName}
             onChange={e => setFirstName(e.target.value)}
+            required
           />
         </label>
         <label className="padLabel">
@@ -30,6 +54,27 @@ const Add = () => {
             type="text"
             value={lastName}
             onChange={e => setLastName(e.target.value)}
+            required
+          />
+        </label>
+        <label className="padLabel">
+          Email:
+          <input
+            className="inputMargin"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label className="padLabel">
+          Cell Phone:
+          <input
+            className="inputMargin"
+            type="tel"
+            value={cellPhone}
+            onChange={e => setCellPhone(e.target.value)}
+            required
           />
         </label>
       </div>
