630b25bd5b7cbbee29339b87a095d43d3b6eca0b
Daniel Simonson (contractor) <BH15491@federated.fds>
2020-02-11 10:16:26 -0500

    finished basic functionality for addForm, posting to firestore with formik validation

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import Autosuggest from "react-autosuggest";
 import { Formik, Field, Form } from "formik";
 import { TextField, Button, Radio, Checkbox } from "@material-ui/core";
 import * as Yup from "yup";
 import axios from "axios";
 import Error from "./Errors";
-import { render } from "react-dom";
-
+import firebase from "../Firestore";
+const nanoid = require("nanoid");
 const ValidationSchema = Yup.object().shape({
   cellPhone: Yup.string()
     .min(1, "Too Short!")
@@ -34,108 +34,125 @@ const ValidationSchema = Yup.object().shape({
     .required("Required")
 });
 
-export default function AddForm() {
-  return (
-    <div>
-      <Formik
-        initialValues={{
-          status: "",
-          active: "",
-          cellPhone: "",
-          firstName: "",
-          lastName: "",
-          name: "",
-          email: "",
-          country: "",
-          postalCode: ""
-        }}
-        validationSchema={ValidationSchema}
-        validate={values => {
-          let errors = {};
+class AddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  postData = data => {
+    const id = nanoid();
+    const db = firebase.firestore();
+    let tempArray = [];
+    tempArray.push(data);
 
-          // Validate the Postal Code conditionally based on the chosen Country
-          /*if (!isValidPostalCode(values.postalCode, values.country)) {
-          errors.postalCode = `${postalCodeLabel(values.country)} invalid`;
-        }*/
+    db.collection("customer")
+      .add({
+        cellPhone: tempArray[0].cellPhone,
+        email: tempArray[0].email,
+        firstName: tempArray[0].firstName,
+        lastName: tempArray[0].lastName,
+        status: tempArray[0].status,
+        userID: id
+      })
+      .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+  render() {
+    return (
+      <div>
+        <Formik
+          initialValues={{
+            status: "",
+            cellPhone: "",
+            firstName: "",
+            lastName: "",
+            email: ""
+          }}
+          validationSchema={ValidationSchema}
+          validate={values => {
+            let errors = {};
+            return errors;
+          }}
+          onSubmit={(data, { setSubmitting }) => {
+            setSubmitting(true);
+            this.postData(data);
+            setSubmitting(false);
+          }}
+        >
+          {({ values, isSubmitting, errors, touched }) => (
+            <Form>
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+              <div className="input-row">
+                <label>First Name</label>
+                <Field
+                  placeholder="First Name"
+                  name="firstName"
+                  type="input"
+                  as={TextField}
+                  className={
+                    touched.firstName && errors.firstName ? "has-error" : null
+                  }
+                />
+                <Error touched={touched.firstName} message={errors.firstName} />
+              </div>
+              <div className="input-row">
+                <label>Last Name</label>
+                <Field
+                  placeholder="Last Name"
+                  name="lastName"
+                  type="input"
+                  as={TextField}
+                  className={
+                    touched.lastName && errors.lastName ? "has-error" : null
+                  }
+                />
+                <Error touched={touched.lastName} message={errors.lastName} />
+              </div>
+              <div className="input-row">
+                <label>Email</label>
+                <Field
+                  placeholder="Email"
+                  name="email"
+                  type="text"
+                  as={TextField}
+                  className={touched.email && errors.email ? "has-error" : null}
+                />
+                <Error touched={touched.email} message={errors.email} />
+              </div>
+              <div className="input-row">
+                <label>Cell Phone</label>
+                <Field
+                  name="cellPhone"
+                  type="input"
+                  as={TextField}
+                  className={
+                    touched.cellPhone && errors.cellPhone ? "has-error" : null
+                  }
+                />
+                <Error touched={touched.cellPhone} message={errors.cellPhone} />
+              </div>
+              <div className="input-row">
+                <label>Status</label>
+              </div>
+              <div className="input-row">
+                <label>Active</label>
+                <Field name="status" type="radio" value="active" as={Radio} />
+                <label>Inactive</label>
+                <Field name="status" type="radio" value="inactive" as={Radio} />
+              </div>
 
-          return errors;
-        }}
-        onSubmit={(data, { setSubmitting }) => {
-          setSubmitting(true);
-          //make async call
-          console.log("submitted data: ", data);
-
-          setSubmitting(false);
-        }}
-      >
-        {({ values, isSubmitting, errors, touched }) => (
-          <Form>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-            <div className="input-row">
-              <label>First Name</label>
-              <Field
-                placeholder="First Name"
-                name="firstName"
-                type="input"
-                as={TextField}
-                className={
-                  touched.firstName && errors.firstName ? "has-error" : null
-                }
-              />
-              <Error touched={touched.firstName} message={errors.firstName} />
-            </div>
-            <div className="input-row">
-              <label>Last Name</label>
-              <Field
-                placeholder="Last Name"
-                name="lastName"
-                type="input"
-                as={TextField}
-                className={
-                  touched.lastName && errors.lastName ? "has-error" : null
-                }
-              />
-              <Error touched={touched.lastName} message={errors.lastName} />
-            </div>
-            <div className="input-row">
-              <label>Email</label>
-              <Field
-                placeholder="Email"
-                name="email"
-                type="text"
-                as={TextField}
-                className={touched.email && errors.email ? "has-error" : null}
-              />
-              <Error touched={touched.email} message={errors.email} />
-            </div>
-            <div className="input-row">
-              <label>Cell Phone</label>
-              <Field
-                name="cellPhone"
-                type="input"
-                as={TextField}
-                className={
-                  touched.cellPhone && errors.cellPhone ? "has-error" : null
-                }
-              />
-              <Error touched={touched.cellPhone} message={errors.cellPhone} />
-            </div>
-            <div className="input-row">
-              <label>Status</label>
-            </div>
-            <div className="input-row">
-              <label>Active</label>
-              <Field name="status" type="radio" value="active" as={Radio} />
-              <label>Inactive</label>
-              <Field name="status" type="radio" value="inactive" as={Radio} />
-            </div>
-
-            <div>
-              <button type="submit">Submit</button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
 }
+export default AddForm;
